5401e862f77b98b8aa925fc06dd9729fe5e59fad
Brian <bmoura@byupathway.edu>
2026-01-05 11:21:05 -0400

    chore: week1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -10,6 +10,7 @@ public static class Arrays
     {
         // TODO Problem 3 Start
         double[] results = new double[length];
+        // Iterate over a results and them multiply it by the index (*1, *2, *3, etc).
         for (int i = 0; i < length; i++)
         {
             results[i] = number * (i + 1);
@@ -30,6 +31,11 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // It iterate over the amount
+        // saves the last element, then
+        // removes the last element from the list
+        // and inserts it at the start of the list.
         for (int i = 0; i < amount; i++)
         {
             int lastElement = data[data.Count - 1];
